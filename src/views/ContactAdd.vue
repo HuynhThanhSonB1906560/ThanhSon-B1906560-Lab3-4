9b66b9771e4d5044edd61150e46c383f18db2df2
HuynhThanhSonB1906560 <sobinson2001@gmail.com>
2022-10-28 12:57:39 +0700

    Tao trang them lien he

---
+++ src/views/ContactAdd.vue
@@ -0,0 +1,29 @@
+<template>
+    <div v-if="contact" class="page">
+        <h4>Thêm Liên hệ</h4>
+        <ContactForm :contact="contact" @submit:contact="createContact"/>
+        <p>{{ message }}</p>
+    </div>
+</template>
+<script>
+import ContactForm from "@/components/ContactForm.vue";
+import ContactService from "@/services/contact.service";
+export default {
+    components: {
+        ContactForm,
+    },
+    data() {
+        return {
+            contact: ContactForm,
+            message: "",
+        };
+    },
+    methods: {
+        async createContact(data) {
+            ContactService.create(data);
+            await alert("Thêm liên hệ thành công");
+            this.$router.push({ name: "contactbook" });
+        }
+    },     
+};
+</script>
